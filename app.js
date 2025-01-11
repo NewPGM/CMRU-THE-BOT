9eab7c8d44cccb6a96614fdd2c0ba30479c07c43
Seerasak Tajai <64143109@g.cmru.ac.th>
2025-01-11 19:09:08 +0700

    d

--- app.js
+++ app.js
@@ -11,8 +11,8 @@ app.use(cors());
 // Use the webhook routes
 app.use('/webhook', webhookRoutes);
 
-// const port = process.env.PORT || 7000;
-// app.listen(port, () => {
-//   console.log(`Server is running on port ${port}`);
-// });
-module.exports = app;
+const port = process.env.PORT || 7000;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
+//
